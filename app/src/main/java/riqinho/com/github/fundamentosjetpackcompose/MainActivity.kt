1059c1bd5c4a74eccba8c0e036ba0ae796769011
Rick Alves Domingues <rickpoterdom@gmail.com>
2025-08-20 22:31:18 -0300

    Linhas, colunas, composables e muito mais

--- app/src/main/java/riqinho/com/github/fundamentosjetpackcompose/MainActivity.kt
+++ app/src/main/java/riqinho/com/github/fundamentosjetpackcompose/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import riqinho.com.github.fundamentosjetpackcompose.ui.theme.FundamentosJetpackComposeTheme
 
@@ -20,8 +25,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             FundamentosJetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TextoCustomizado(
+                        "Primeira função composable",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Inicial(modifier: Modifier = Modifier){
+    Column (modifier = modifier.background(color = Color.Blue)) {
+        TextoCustomizado("Primeira função composable")
+        Row {
+            TextoCustomizado("Seja bem vindo!")
+            TextoCustomizado("Olá usuário.")
+        }
+    }
+}
+
+@Composable
+fun TextoCustomizado (texto : String, modifier : Modifier = Modifier){
     Text(
-        text = "Hello $name!",
+        text = texto,
+        color = Color.White,
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FundamentosJetpackComposeTheme {
-        Greeting("Android")
+fun PreviewInicial(){
+    FundamentosJetpackComposeTheme  {
+        Inicial()
     }
 }
