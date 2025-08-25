7245d51642ae9e0e183e6ab297d6146eb5e82ea6
Rick Alves Domingues <rickpoterdom@gmail.com>
2025-08-24 21:16:28 -0300

    finalizando o projeto

--- app/src/main/java/riqinho/com/github/fundamentosjetpackcompose/componentes/Text.kt
+++ app/src/main/java/riqinho/com/github/fundamentosjetpackcompose/componentes/Text.kt
@@ -2,7 +2,9 @@ package riqinho.com.github.fundamentosjetpackcompose.componentes
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,12 +13,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import riqinho.com.github.fundamentosjetpackcompose.R
+import java.nio.file.WatchEvent
 
 
 @Composable
@@ -53,8 +60,49 @@ fun TextShadow(){
     }
 }
 
+@Composable
+fun DifferentFont(){
+    val fontFamilyEdu = FontFamily(
+         Font(R.font.edu_nswact_cursive_regular, FontWeight.Normal),
+         Font(R.font.edu_nswact_cursive_medium, FontWeight.Medium),
+         Font(R.font.edu_nswact_cursive_semibold, FontWeight.SemiBold),
+         Font(R.font.edu_nswact_cursive_bold, FontWeight.Bold),
+    )
+
+    Column {
+        Text(text = stringResource(R.string.hello_turma), fontFamily = FontFamily.Serif)
+        Text(text = stringResource(R.string.hello_turma), fontFamily = FontFamily.Monospace)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "Edu NSWACT Cursive regular. Eu preciso colocar um texto gigante neste local!",
+            fontFamily = fontFamilyEdu,
+            fontWeight = FontWeight.Normal,
+            maxLines = 1
+        )
+        Text(
+            text = "Edu NSWACT Cursive medium. Eu preciso colocar outro texto gigante neste local!",
+            fontFamily = fontFamilyEdu,
+            fontWeight = FontWeight.Medium,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = "Edu NSWACT Cursive semibold",
+            fontFamily = fontFamilyEdu,
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(
+            text = "Edu NSWACT Cursive bold",
+            fontFamily = fontFamilyEdu,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
 @Preview
 @Composable
 fun Preview() {
-    TextShadow()
+    DifferentFont()
 }
